3ac989fbc36cc3d39382fb8d06168c8be837ac1a
RifkyAK <113667418+Rakaruan14@users.noreply.github.com>
2024-09-20 20:39:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('GET WELL SOON BANG AL ♥').split('')
+    const titles = ('I LOVE YOU ♥').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
