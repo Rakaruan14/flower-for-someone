d3f343b8588a8887063d38d642f3f70607157fcc
RifkyAK <113667418+Rakaruan14@users.noreply.github.com>
2024-10-20 19:04:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE YOU ♥').split('')
+    const titles = ('THE DEEP OF MY HEART IM TRULY SORRY KA ALFA ♥').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
